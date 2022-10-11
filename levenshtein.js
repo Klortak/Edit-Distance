85a03f5ca29ee6b2568c02295974da3604d4ce1d
Klortak <84869525+Klortak@users.noreply.github.com>
2022-10-11 07:08:24 -0500

    Add files via upload

--- levenshtein.js
+++ levenshtein.js
@@ -20,166 +20,165 @@ function clamp(num, min, max) {
 }
 
 // Returns a number based on a keyboard key's distance from another
-function add_weight(target, string, min = 0, max = 1) {
-  // If parameters are unset the function returns the max weight
-  if(!target || !string){
-    return 1;
+function add_keyboard_influence(a, b, min = 0, max = 1) {
+  // If parameters are unset the function returns the minimum weight
+  if(a == ' ' || b == ' '){
+    return 0;
   }
 
   // A matrix of the keys on a keyboard
   keyboard = [
-    '~` !1 @2 #3 $4 %5 ^6 &7 *8 (9 )0 _- += ㅤ ㅤ ',
-    'ㅤㅤ Qq Ww Ee Rr Tt Yy Uu Ii Oo Pp {[ }] |\\ ',
-    'ㅤㅤ  Aa Ss Dd Ff Gg Hh Jj Kk Ll :; \"\' ㅤ ㅤ',
-    'ㅤㅤ ㅤ Zz Xx Cc Vv Bb Nn Mm <, >. ?/ ㅤ ㅤ ㅤ '
+    ['~`', '!1', '@2', '#3', '$4', '%5', '^6', '&7', '*8', '(9', ')0', '_-', '+='],
+    [0, 'Qq', 'Ww', 'Ee', 'Rr', 'Tt', 'Yy', 'Uu', 'Ii', 'Oo', 'Pp', '{[', '}]', '|\\'],
+    [0, 0, 'Aa', 'Ss', 'Dd', 'Ff', 'Gg', 'Hh', 'Jj', 'Kk', 'Ll', ':;', '\"\''],
+    [0, 0, 0, 'Zz', 'Xx', 'Cc', 'Vv', 'Bb', 'Nn', 'Mm', '<,', '>.', '?/']
   ];
 
   // Declare points
-  let x1;
-  let x2;
-  let y1;
-  let y2;
+  let x1, y1, x2, y2;
 
   // For each row
   for(x = 0; x < keyboard.length; x++) {
     // For each element in the row
-    for(y = 0; y < keyboard[x].split(' ').length; y++) {
-      let key = keyboard[x].split(' ')[y];
-      // Set variables if key matches main or alternative key
-      if(key[0] == target || key[1] == target) {
+    for(y = 0; y < keyboard[x].length; y++) {
+
+      let key = keyboard[x][y][0];
+      let alt = keyboard[x][y][1];
+
+      // Does a check for the main key and the alternative one
+
+      // Set coordinate if key matches
+      if(key == a || alt == a) {
         x1 = x;
         y1 = y;
       }
 
-      // Set variables if key matches main or alternative key
-      if(key[0] == string || key[1] == string) {
+      // Set coordinate if key matches
+      if(key == b || alt == b) {
         x2 = x;
         y2 = y;
       }
     }
   }
 
-  // Calculate weight by using the distance formula
+  // Calculate the distance between the two points
   let x3 = Math.pow(x2 - x1, 2);
   let y3 = Math.pow(y2 - y1, 2);
   let d = Math.sqrt(x3 + y3);
 
-  // The min and max distance apart from a key
-  const min_distance = 1
-  const max_distance = 10
+  // 12.37 appears to be the maximum distance this formula returns
+  // Maps the distance to a range between the given parameters min and max to be used as the weight
+  let md = map(d, 0, 12.37, min, max);
 
-  // Clamp
-  let cd = clamp(d, min_distance, max_distance);
-  // Map range
-  let md = map(cd, min_distance, max_distance, min, max);
-
-  // Math.pow return NaN if the number is a negative
-  // This is a failsafe
-  if(!x3 || !y3) {
-    return 1;
-  }
-
+  // Returns the weight rounded to the nearest two decimal places
+  // toFixed returns a string
+  // The plus in front converts it to a number
   return +md.toFixed(2);
 }
 
 // Takes a single target and compares it to a possible match and returns the statistics
-function levenshtein(target, string, weighted = false, case_sensitive = false) {
+function levenshtein(input, target, weighted = false, case_sensitive = false) {
+
   // If both parameters are empty return a default object
-  if(!target && !string) {
+  if(!input && !target) {
     return create_object();
   }
 
   // If the target is empty return the length of the string to be compared to
-  if(!target) {
-    return create_object(string, 0, string.length);
-  }
-
-  // If the string to be compared to is empty return the length of the target
-  if(!string) {
+  if(!input) {
     return create_object(target, 0, target.length);
   }
 
+  // If the string to be compared to is empty return the length of the target
+  if(!target) {
+    return create_object(input, 0, input.length);
+  }
+
+  // Will save the original target as a precaution to it being lowercased
+  let original_target = target;
+
   // Lowercase both strings if case insensitive
   if(!case_sensitive) {
-    original_string = string;
-
+    input = input.toLowerCase();
     target = target.toLowerCase();
-    string = string.toLowerCase();
   }
 
   let matrix = [];
 
   // X Axis
-  for(i = 0; i <= target.length; i++) {
+  for(i = 0; i <= input.length; i++) {
     matrix[i] = [i];
   }
 
   // Y Axis
-  for(j = 0; j <= string.length; j++) {
+  for(j = 0; j <= target.length; j++) {
     matrix[0][j] = j;
   }
 
   // Fill in the rest of the matrix
-  for(i = 1; i <= target.length; i++) {
-    for(j = 1; j <= string.length; j++) {
+  for(i = 1; i <= input.length; i++) {
+    for(j = 1; j <= target.length; j++) {
       // If the characters are the same, don't change the value
-      if(target.charAt(i - 1) == string.charAt(j - 1)) {
+      if(input.charAt(i - 1) == target.charAt(j - 1)) {
         matrix[i][j] = matrix[i - 1][j - 1];
       }
       // Otherwise set it to the minimum of the three values 
       else {
-        let weight = add_weight(target.charAt(i - 1), string.charAt(j - 1))
+        let weight = weighted ? 1 + add_keyboard_influence(input.charAt(i - 1), target.charAt(j - 1)) : 1;
         matrix[i][j] = Math.min(
           matrix[i - 1][j - 1],
           matrix[i][j - 1],
           matrix[i - 1][j]
-        ) + weighted ? weight : 1;
+        ) + weight
       }
     }
   }
 
   // Levenshtein distance
-  let distance = matrix[target.length][string.length];
-  let percentage = 100 - (distance / target.length) * 100;
+  let distance = matrix[input.length][target.length];
+  //let percentage = 100 - (distance / string.length) * 100;
+  let percentage = 0;
 
   // Return an object with the original string, percentage, and distance
-  return create_object(original_string, percentage, distance);
+  return create_object(original_target, percentage, distance);
 }
 
 // Takes a single target and compares it against multiple possible matches and returns the best match
-function batch_levenshtein(target, strings, case_sensitive = false, return_all = false) {
+function batch_levenshtein(input, targets, weighted = false, case_sensitive = false, return_all = false) {
   let results = [];
   let percentages = [];
   // For each string to compare
-  for(let i = 0; i < strings.length; i++) {
+  for(let i = 0; i < targets.length; i++) {
     // Get levenshtein distance
-    l = levenshtein(target, strings[i], case_sensitive);
+    l = levenshtein(input, targets[i], weighted, case_sensitive);
     // Compile results
     results[i] = create_object(l.string, l.percentage, l.distance);
     // Add percentage to array
     percentages.push(l.percentage);
   }
 
-  // If return all is true, return all the results
-  if(return_all) return results;
+  // If return all is true this will return the stats for all of the words provided
+  if(return_all) {
+    results.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
+    return results;
+  }
 
-  // Get highest percentage
-  let highest = Math.max(...percentages);
+
+  let closest = results[0];
 
   // For each result
   for (let i = 0; i < results.length; i++) {
-    // If the highest percentage is the same as the current percentage
-    if(highest == results[i].percentage) {
-      // Return corresponding object
-      return results[i];
+    // If the distance is lower than the current lowest then set the object to be the closest
+    if(results[i].distance < closest.distance) {
+      closest = results[i];
     }
   }
+  // Return the object with the lowest distance
+  return closest;
 }
-
-// Returning different values
-//console.log(levenshtein('bonk', 'bonk1'))
-//console.log(levenshtein('bonk1', 'bonk'))
-
-// Returning different values
-//console.log(levenshtein('bonk', 'bonk1', true))
-//console.log(levenshtein('bonk1', 'bonk', true))
+// Add in character repetition detection
+// All should return the same value
+//console.log(levenshtein('bonk', 'bbonk', true))
+//console.log(levenshtein('bonk', 'boonk', true))
+//console.log(levenshtein('bonk', 'bonnk', true))
+//console.log(levenshtein('bonk', 'bonkk', true))
